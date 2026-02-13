27223b081770e67a66df7eb5dde19920d42f553c
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-13 13:21:09 +0700

    membuat skor

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);   
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(                   
-      title: 'Fruit Catcher Game',
-      home: GameScreen(),
-    );
+    return const MaterialApp(title: 'Fruit Catcher Game', home: GameScreen());
   }
 }
 
-
 class GameScreen extends StatefulWidget {
- const GameScreen({super.key});
- @override
- State<GameScreen> createState() => _GameScreenState();
+  const GameScreen({super.key});
+  @override
+  State<GameScreen> createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
- @override
- void initState() {
- super.initState();
- }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(10),
+              ),
 
-
+              child: const Text(
+                'Score: 1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ), //Container
+          ), // Positioned
+        ],
+      ),
+    );
+  }
+}
