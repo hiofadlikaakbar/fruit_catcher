2440430abe09a7212dfc237ceb5e3e9461dec291
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-14 18:34:55 +0700

    merubah child menjadi valuelistenbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -47,13 +47,18 @@ class _GameScreenState extends State<GameScreen> {
                 borderRadius: BorderRadius.circular(10),
               ),
 
-              child: const Text(
-                'Score: 1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: ValueListenableBuilder<int>(
+                valueListenable: counter,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
               ),
             ), //Container
           ),
