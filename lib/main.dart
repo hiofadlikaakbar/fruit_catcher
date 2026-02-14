0b91ceb35d58815ab5ebf7b206458cc6eb4c559f
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-14 19:00:53 +0700

    membungkus dengan childeren expanded dan merubah body jadi column

--- lib/main.dart
+++ lib/main.dart
@@ -35,52 +35,60 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
-              ),
-
-              child: ValueListenableBuilder<int>(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    'Score: $score',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-            ), //Container
-          ),
-
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
+          Expanded(
+            child: Stack(
               children: [
-                IconButton(
-                  icon: const Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {
-                    counter.value++;
-                  },
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+
+                    child: ValueListenableBuilder<int>(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {},
+
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.music_note, color: Colors.black),
+                        onPressed: () {
+                          counter.value++;
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.volume_up, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ), // Positioned
+          ),
+          // expanded agar layar ambil sisa
+          // Positioned
         ],
       ),
     );
