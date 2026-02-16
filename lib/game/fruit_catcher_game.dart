c367364b363a52c04662caeaa21dc60ae8c625ef
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-17 00:55:59 +0700

    menambah beberapa fungsi di catcher fruit

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -9,9 +9,7 @@ import 'components/basket.dart';
 import 'components/fruit.dart';
 import 'managers/audio_manager.dart';
 
-class FruitCatcherGame extends FlameGame
-    with HasCollisionDetection {
-
+class FruitCatcherGame extends FlameGame with HasCollisionDetection {
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
 
@@ -23,3 +21,35 @@ class FruitCatcherGame extends FlameGame
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+
+    AudioManager().playBackgroundMusic();
+
+    add(Basket());
+
+    _spawnTimer = Timer(1, repeat: true, onTick: spawnFruit);
+
+    _spawnTimer.start();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _spawnTimer.update(dt);
+  }
+
+  void spawnFruit() {
+    final xPosition = _random.nextDouble() * size.x;
+
+    add(Fruit(position: Vector2(xPosition, -20)));
+  }
+
+  void incrementScore() {
+    scoreNotifier.value++;
+  }
+
+  @override
+  void onRemove() {
+    scoreNotifier.dispose();
+    super.onRemove();
+  }
+}
