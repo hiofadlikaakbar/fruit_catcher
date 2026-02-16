f61b9635437b7cde02b42131996faa34760adf5a
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-17 00:58:43 +0700

    membuat position component di fruit dart

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -2,3 +2,52 @@ import 'dart:math';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import '../fruit_catcher_game.dart';
+import 'basket.dart';
+
+enum FruitType { apple, banana, orange, strawberry }
+
+class Fruit extends PositionComponent
+    with HasGameRef<FruitCatcherGame>, CollisionCallbacks {
+
+  final FruitType type;
+  final double fallSpeed = 200;
+  final Random random = Random();
+
+  Fruit({super.position})
+      : type = FruitType.values[
+            Random().nextInt(FruitType.values.length)],
+        super(size: Vector2.all(40));
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+    add(CircleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    
+    position.y += fallSpeed * dt;
+
+   
+    if (position.y > gameRef.size.y + 50) {
+      removeFromParent();
+    }
+  }
+
+  @override
+  void onCollision(
+      Set<Vector2> intersectionPoints,
+      PositionComponent other,
+  ) {
+    super.onCollision(intersectionPoints, other);
+
+    if (other is Basket) {
+      gameRef.incrementScore();
+      removeFromParent();
+    }
+  }
