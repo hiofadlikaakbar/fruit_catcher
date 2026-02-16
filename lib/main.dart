60e29438a54e8c4324fd920ddaca9631272ba3e2
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-17 00:19:24 +0700

    konfigurasi main dart dengan fruit catcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'game/fruit_catcher_game.dart';
 import 'game/managers/audio_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize audio sekali saat aplikasi start
   await AudioManager().initialize();
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Fruit Catcher Game', home: GameScreen());
-  }
-}
-
-class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
-  @override
-  State<GameScreen> createState() => _GameScreenState();
-}
-
-class _GameScreenState extends State<GameScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  final ValueNotifier<int> counter = ValueNotifier(1);
-  @override
-  void dispose() {
-    counter.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
-              ),
-
-              child: ValueListenableBuilder<int>(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    'Score: $score',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {
-                    AudioManager().toggleMusic();
-                  },
-                ),
-
-                IconButton(
-                  icon: const Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {
-                    AudioManager().toggleSfx();
-                  },
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 30,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: ElevatedButton(
-                onPressed: () {
-                  counter.value++;
-                },
-                child: const Text("Tambah Score"),
-              ),
-            ),
-          ),
-        ],
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
     );
   }
 }
