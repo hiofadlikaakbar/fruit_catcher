6dd2e17fa59ea38705047d997d94618680abdfd4
hiofadlikaakbar <hioakbar1@gmail.com>
2026-02-17 00:52:22 +0700

    fixing fruit catcher

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,24 +1,25 @@
+import 'dart:async';
+import 'dart:math';
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/collisions.dart';
 import 'package:flutter/material.dart';
+import 'components/basket.dart';
+import 'components/fruit.dart';
 import 'managers/audio_manager.dart';
 
-class FruitCatcherGame extends FlameGame {
+class FruitCatcherGame extends FlameGame
+    with HasCollisionDetection {
+
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
 
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
 
+  late Timer _spawnTimer;
+  final Random _random = Random();
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-
-    AudioManager().playBackgroundMusic();
-  }
-
-  @override
-  void onRemove() {
-    scoreNotifier.dispose();
-    super.onRemove();
-  }
-}
